feb608a8afbbd031e607837595833b04ed66a5dc
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-09-05 21:53:32 +0530

    Update and rename VariableTransformation.java to guvi3.java

--- guvi3.java
+++ guvi3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VariableTransformation {
+public class guvi3{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
